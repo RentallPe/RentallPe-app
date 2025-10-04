de329ae28680cdc3fd5a5bc4758b88f9bb8c0915
Thunder <artax1605@gmail.com>
2025-10-04 16:00:43 -0500

    first

---
+++ src/router.js
@@ -0,0 +1,37 @@
+import {createRouter, createWebHistory} from "vue-router";
+//import Home from "./shared/presentation/views/home.vue";
+//import publishingRoutes from './publishing/presentation/publishing-routes.js';
+/*
+*
+* const about = () => import('./shared/presentation/views/about.vue');
+const pageNotFound = () => import('./shared/presentation/views/page-not-found.vue');
+
+
+const routes = [
+    { path: "/home",                name: 'home',       component: Home,            meta: {title: 'Home'}},
+    { path: "/about",               name: 'about',      component: about,           meta: {title: 'About'}},
+    { path: '/publishing',          name: 'publishing', children: publishingRoutes},
+    { path: "/",                    redirect: "/home"},
+    { path: "/:pathMatch(.*)*",     name: 'not-found',  component: pageNotFound,    meta: {title: 'Page Not Found'}}
+];
+
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: routes
+});
+
+
+router.beforeEach((to, from, next) => {
+    console.log(`Navigating from ${from.name} to ${to.name}`);
+    let baseTitle = 'ACME Learning Center';
+    document.title = to.meta.title ? `${to.meta.title} | ${baseTitle}` : baseTitle;
+    next();
+});
+
+
+* */
+const router=[
+
+]
+
+export default router;
